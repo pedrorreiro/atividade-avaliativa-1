f9996010e518b87f3aaff8d709a744c9564b0739
Pedro <pedro_rique17@hotmail.com>
2021-11-11 11:34:02 -0300

    corrigindo datas

--- test/fatura.test.js
+++ test/fatura.test.js
@@ -55,9 +55,9 @@ describe("Filtrar salario", () => {
       c2 = new Cliente("Matheus", new Date(2021,11,2), "Minas Gerais");
       c3 = new Cliente("Jorge", new Date(2021,10,8), "São Paulo");
 
-      f1 = new Fatura(0,2499,new Date(2021,11,5), c1);
-      f2 = new Fatura(0,2100,new Date(2021,10,21), c2);
-      f3 = new Fatura(0,2200,new Date(2021,10,5), c3);
+      f1 = new Fatura(0,2499,new Date(2021,10,5), c1);
+      f2 = new Fatura(0,2100,new Date(2021,9,21), c2);
+      f3 = new Fatura(0,2200,new Date(2021,9,5), c3);
 
       const faturas = [f1,f2,f3];
 
